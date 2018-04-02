6829593bddd39b141b22c8c79dd69a12b7ce779e
Matbit <codesharing@gmx.de>
2018-04-02 20:21:05 +0200

    Shares -0001

---
+++ Shares/src/shares/data/Data.java
@@ -0,0 +1,5 @@
+package shares.data;
+
+public class Data {
+    
+}
---
+++ Shares/src/shares/program/Shares.java
@@ -0,0 +1,11 @@
+package shares.program;
+import shares.model.Share;
+
+public class Shares {
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
