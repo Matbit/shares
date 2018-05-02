989d273d198cdcb53404a5cfba6b836173a70b8a
Matbit <codesharing@gmx.de>
2018-05-02 20:30:51 +0200

    Shares 1.5.4 -0055
    fixed bug (company name)

--- Shares/src/shares/model/Share.java
+++ Shares/src/shares/model/Share.java
@@ -11,6 +11,8 @@ public class Share {
     public Share(){
         marketValue = initStartValue();
         name = setName();
+        //TO DO: clear 
+        System.out.println(name);
     }
     
     private double initStartValue(){
@@ -25,12 +27,14 @@ public class Share {
     private String setName(){
         
         int firstTry = (int) (Math.random() * 8);
+        //TO DO clear
+        System.out.println(firstTry);
         
         //check whether company still exists
         if(!companyList.isEmpty()){
             for(int saveFirst = firstTry; saveFirst==firstTry;){
                 if(companyList.contains(firstTry)){
-                firstTry = (int) (Math.random()*10);
+                firstTry = (int) (Math.random()*8);
                 }
                 else{
                     saveFirst = -1;
