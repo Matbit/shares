65d92eb730f2dcb47a5ad8d1874ac5f45f7614b2
Matbit <codesharing@gmx.de>
2018-05-27 20:06:26 +0200

    2.7.6 -0065
    added new behavior

--- service/ServiceValueCalculation.java
+++ service/ServiceValueCalculation.java
@@ -16,13 +16,36 @@ public class ServiceValueCalculation {
     public static double shareManagementSystem(){
         ShareMode currentMode = mode;
         if(currentMode == ShareMode.NORMAL_MODE){
-            
-            
-            
+            return calcNormalMode();  
+        }
+        if(currentMode == ShareMode.POWER_MODE){
+            return calcPowerMode();
+        }
+        if(currentMode == ShareMode.SUICIDE_MODE){
+            return calcSuicideMode()*(-1);
+        }
+        return 0d;
+    }
+    
+    private static void changeMode(){
+        ShareMode currentMode = mode;
+        ShareMode newMode = mode;
+        
+        while(currentMode == newMode){
+            double newTry = (int) (Math.random()*4);
+            if(newTry == 0){
+                newMode = ShareMode.SUICIDE_MODE;
+            }
+            if(newTry == 1){
+                newMode = ShareMode.POWER_MODE;
+            }
+            if(newTry > 1){
+                newMode = ShareMode.NORMAL_MODE;
+            }
         }
         
-        //....
-        return 0d;
+        mode = newMode;
+        
     }
     
     private static double calcNormalMode(){
@@ -32,7 +55,7 @@ public class ServiceValueCalculation {
         //change mode if maximum is reached
         if(periodDay >= length){
             counter = 0;
-            //
+            changeMode();
         }
         
         //if first day in normal mode        
@@ -45,8 +68,62 @@ public class ServiceValueCalculation {
         return calcNewRateB(Data.getCurrentBShare());
     }
     
+    private static double calcPowerMode(){
+        int length = 8;
+        int periodDay = counter;
+        
+        //change mode if maximum is reached
+        if(periodDay >= length){
+            counter = 0;
+            changeMode();
+        }
+        
+        //if first day in normal mode        
+        if(counter == 0){
+            calcModePeriod(length);
+        }
+        //count +1
+        counter++;
+        
+        double rate = (int) (Math.random()*100);
+        
+        if(rate > 10){
+            return rate * 1.98/100;
+        }
+        if(rate > 20){
+            return rate*2.16/100;
+        }
+        if(rate > 30){
+            return rate*2.62/100;
+        }
+        if(rate > 40){
+            return rate *2.98/100;
+        }
+        if(rate > 50){
+            return rate*3.45/100;
+        }
+        if(rate > 60){
+            return rate*3.80/100;
+        }
+        if(rate > 70){
+            return rate *4.37/100;
+        }
+        if(rate > 80){
+            return rate * 5.83/100;
+        }
+        if(rate > 90){
+            return rate * 6.25/100;
+        }
+        if(rate > 89){
+            return rate * 6.8/100;
+        }
+        return rate;
+        
+    }
+    
+       
     private static void calcModePeriod(int maximum){
-        maximumDays = (int) Math.random()* maximum;
+        maximumDays = (int) Math.random()* maximum +(+1);
     }
     
     //calculate new share rate (a)
@@ -137,9 +214,23 @@ public class ServiceValueCalculation {
     }			
     
     private static double calcSuicideMode(){
-        
-        
         int length = 5;
+        int periodDay = counter;
+        
+        //change mode if maximum is reached
+        if(periodDay >= length){
+            counter = 0;
+            changeMode();
+        }
+        
+        //if first day in suicide mode        
+        if(counter == 0){
+            calcModePeriod(length);
+        }
+        //count +1
+        counter++;
+        
+        
         double rate = (int) (Math.random()*100);
         
         if(rate > 10){
